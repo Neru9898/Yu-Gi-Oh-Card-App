ddd0ff76bb846b383b4e04b88c37367c8b5fe104
Nerushan Naguleswaran <neonerushan@gmail.com>
2021-10-11 18:29:41 -0400

    Displaying Card Info

--- client/src/Components/DisplayCardsHorizontal.js
+++ client/src/Components/DisplayCardsHorizontal.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./DisplayCardsHorizontal.css";
 export const DisplayCardsHorizontal = ({ cardListToDisplay }) => {
   return (
     <Carousel fade={true} nextLabel={null} prevLabel={null} interval={null}>
@@ -8,6 +9,15 @@ export const DisplayCardsHorizontal = ({ cardListToDisplay }) => {
         return (
           <Carousel.Item key={card.id}>
             <img src={card.card_images[0].image_url} alt={card.name} />
+
+            <h3>Name: {card.name}</h3>
+            <h4>Archetype: {card.archetype}</h4>
+            <h4>
+              Sets:
+              {card.card_sets
+                ? `  ${card.card_sets.map((set) => set.set_name)} `
+                : "RAH"}
+            </h4>
           </Carousel.Item>
         );
       })}
