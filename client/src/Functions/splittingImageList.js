77262eceab69eca3add3633b95ce394f2435b01b
Neru9898 <neonerushan@gmail.com>
2022-08-27 15:36:37 -0400

    Removed Front Temp

--- client/src/Functions/splittingImageList.js
+++
@@ -1,8 +0,0 @@
-export const splittingImageList = (
-  cardListToDisplay,
-  startIndex,
-  increment
-) => {
-  const imageList = cardListToDisplay.slice(startIndex, increment);
-  return imageList;
-};
