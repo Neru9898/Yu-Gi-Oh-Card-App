57821fc8799a525afebf4675956edd3d7dce634f
Neru9898 <neonerushan@gmail.com>
2022-08-27 16:15:41 -0400

    Created simple signup

--- Server/index.js
+++ Server/index.js
@@ -2,9 +2,21 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const PORT = 3000;
-
+const userList = [];
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.post("/signup", (req, res) => {
+  const newUser = req.body;
+  const findUser = userList.find((user) => user.name === newUser.name);
 
+  if (!findUser) {
+    userList.push(newUser);
+    res.status(201).json({ success: true, msg: "User has been created" });
+  } else {
+    res.status(400).json({ success: false, msg: "User already exist" });
+  }
+  console.log(userList);
+});
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
